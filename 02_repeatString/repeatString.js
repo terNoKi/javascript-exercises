18e7ddf779a35ba294c3cc8e444c48a938e99b70
terNoKi <polik0ternoki@gmail.com>
2023-03-17 00:34:52 +0100

    First 2 excersizes done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+
+const repeatString = function(word, times) {
+    if (times < 0){
+        let errorMessage = "ERROR"
+        return errorMessage
+    }
+    let string = ""
+    for ( i = 1; i <= times; i++){
+        string += word
+    }
+    return string
 
 };
 
