a46c75a2305593ceef64a4aea766c14463d2a607
terNoKi <polik0ternoki@gmail.com>
2023-03-21 21:44:44 +0100

    done more

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
 
-};
+const findTheOldest = function(people) {
+    let birth = [];
+    let death = [];
+    let age = [];
+    let oldest = [];
+    const date = new Date().getFullYear()
+    for (i = 0; i < people.length; i++){
+        birth[i] = people[i].yearOfBirth;
+        death[i] = people[i].yearOfDeath || date;
+        age[i] = death[i] - birth[i];
+    }
+    let maxAge = Math.max(...age);
+    for (i = 0; i < people.length; i++){
+        if (age[i] === maxAge){
+            oldest.push(people[i])
+        }
+    }
+    let name = oldest[0].name;
+    return name
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
